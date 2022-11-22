f2c06c515e1e0bbbfedf41de5b641c297919a2e0
German Gribanov <germangribanov@mail.ru>
2022-11-23 01:46:31 +0300

    download section event and project

--- Blanchard/JS/swiperSlider.js
+++ Blanchard/JS/swiperSlider.js
@@ -1,4 +1,31 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.gallery__swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  spaceBetween: 50,
+
+});
+
+const swiperEvent = new Swiper('.event__swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+  spaceBetween: 48,
+
+});
+
+const swiperProjects = new Swiper('.projects__swiper', {
   // Optional parameters
   loop: true,
   slidesPerView: 3,
