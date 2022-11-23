19bde9987473589606aa13f520fdfdb47e318a85
German Gribanov <germangribanov@mail.ru>
2022-11-23 21:13:28 +0300

    download section footer

--- Blanchard/JS/index.js
+++ Blanchard/JS/index.js
@@ -1,1 +1,30 @@
 new Accordion('.accordion-container');
+
+type = "text/javascript" >
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [55.758468, 37.601088],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 15,
+    controls: [],
+  });
+
+  var myPlacemark = new ymaps.Placemark([55.758468, 37.601088], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: './img/mapMarker.svg',
+    iconImageSize: [20, 20],
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
+
+
