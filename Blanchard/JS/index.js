1c51956c36ec56d3b0aee79c1dfe631012cf7f3e
German Gribanov <germangribanov@mail.ru>
2022-11-27 23:07:29 +0300

    download biggest part hover,focus effects. Burger, Search-menu, 1024-Media. Desktop pixelPerf

--- Blanchard/JS/index.js
+++ Blanchard/JS/index.js
@@ -1,22 +1,25 @@
 new Accordion('.accordion-container');
 
 type = "text/javascript" >
-  // Функция ymaps.ready() будет вызвана, когда
-  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
   ymaps.ready(init);
+
 function init() {
-  // Создание карты.
+
   var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
     center: [55.758468, 37.601088],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 15,
+    zoom: 14,
     controls: [],
-  });
+  },
+    {
+      suppressMapOpenBlock: true,
+      geolocationControlSize: "large",
+      geolocationControlPosition: { top: "200px", right: "20px" },
+      geolocationControlFloat: 'none',
+      zoomControlSize: "small",
+      zoomControlFloat: "none",
+      zoomControlPosition: { top: "120px", right: "20px" }
+    }
+  );
 
   var myPlacemark = new ymaps.Placemark([55.758468, 37.601088], {}, {
     iconLayout: 'default#image',
@@ -28,3 +31,159 @@ function init() {
 }
 
 
+// dropdowns
+const dropdownButtons = document.querySelectorAll('.main__btn');
+const boxes = document.querySelectorAll('.scrollbox');
+
+document.addEventListener('click', function (event) {
+  dropdownButtons.forEach(function (element) {
+    element.classList.remove('main__btn--active');
+  });
+  boxes.forEach(function (element) {
+    element.classList.remove('scrollbox--active');
+  });
+
+  const targetClassList = [...event.target.classList];
+
+  if (targetClassList.includes('main__btn')) {
+    event.target.classList.toggle('main__btn--active');
+    event.target.nextElementSibling.classList.toggle('scrollbox--active');
+  }
+})
+
+
+// Hamburger
+
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+let headerNavigation = document.querySelector('.header__nav')
+// On click
+hamburger.addEventListener("click", function () {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+  headerNavigation.classList.toggle('header__nav--active')
+});
+
+// gallery
+
+// let tabHoverGallery = document.querySelector('.gallery__hover');
+// let tabHoverBox = document.querySelector('.gallery__hoverbox');
+
+// tabHoverGallery.addEventListener('click', () {
+//   tabHoverBox.classList.toggle('gallery__hoverbox--active')
+// })
+
+// document.addEventListener('click', function (event) {
+//   tabHoverGallery.forEach(function (element) {
+//     element.classList.remove('gallery__hoverbox--active')
+//   }),
+//     tabHoverBox.forEach(function (element) {
+//       element.classList.remove('gallery__hover--active')
+//     }),
+
+//     const targetClassLists = [...event.target.classList];
+
+//   if (targetClassLists.includes('gallery__hover')) {
+//     event.target.classList.toggle('gallery__hoverbox--active');
+//   }
+
+// })
+
+
+// Catalogue
+let tabsBtn = document.querySelectorAll('.catalogue__btn');
+let tabsCatalogueAboutPainter = document.querySelectorAll('.catalogue__aboutpainter');
+
+tabsBtn.forEach(function (element) {
+  element.addEventListener('click', function (e) {
+    const path = e.currentTarget.dataset.path;
+
+    tabsBtn.forEach(function (btn) { btn.classList.remove('catalogue__btn--active') });
+    e.currentTarget.classList.add('catalogue__btn--active');
+
+    tabsCatalogueAboutPainter.forEach(function (item) { item.classList.remove('catalogue__aboutpainter--active') });
+    document.querySelector(`[data-target="${path}"]`).classList.add('catalogue__aboutpainter--active');
+  })
+})
+
+
+// tooltip
+
+let tooltipBtnTab = document.querySelectorAll('.projects__tooltipbtn')
+
+tippy(tooltipBtnTab, {
+  content: 'Wow!!! here is very interesting information'
+})
+
+
+// Smooth Scroll
+
+// document.querySelectorAll('.js-scroll-link').forEach(link => {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const href = this.getAttribute('href').substring(1);
+//     const scrollTarget = document.getElementById(href);
+//     const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+//     window.scrollBy({
+//       top: elementPosition,
+//       behavior: 'smooth'
+//     });
+//   });
+
+
+// Search Form
+
+// здесь мы вызываем функцию и передаем в нее классы наших элементов
+function setSearch(params) {
+  const openBtn = document.querySelector(`.${params.openBtnClass}`);
+  const search = document.querySelector(`.${params.searchClass}`);
+  const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
+
+  search.addEventListener("animationend", function (evt) {
+    if (this._isOpened) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+      this._isOpened = false;
+    } else {
+      this._isOpened = true;
+    }
+  });
+
+  search.addEventListener('click', function (evt) {
+    evt._isSearch = true;
+  });
+
+  openBtn.addEventListener("click", function (evt) {
+    this.disabled = true;
+
+    if (
+      !search.classList.contains(params.activeClass) &&
+      !search.classList.contains(params.hiddenClass)
+    ) {
+      search.classList.add(params.activeClass);
+    }
+  });
+
+  closeBtn.addEventListener('click', function () {
+    openBtn.disabled = false;
+    search.classList.add(params.hiddenClass);
+  });
+
+  document.body.addEventListener('click', function (evt) {
+    if (!evt._isSearch && search._isOpened) {
+      openBtn.disabled = false;
+      search.classList.add(params.hiddenClass);
+    }
+  });
+}
+
+setSearch({
+  openBtnClass: "js-open-search", // класс кнопки открытия
+  closeBtnClass: "js-close", // класс кнопки закрытия
+  searchClass: "js-form", // класс формы поиска
+  activeClass: "is-opened", // класс открытого состояния
+  hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
+});
--- Blanchard/JS/swiperSlider.js
+++ Blanchard/JS/swiperSlider.js
@@ -1,15 +1,32 @@
-const swiper = new Swiper('.gallery__swiper', {
-  // Optional parameters
+var swiper = new Swiper(".gallery__swiper", {
+
   loop: true,
   slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 49,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  breakpoints: {
+
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 34,
+      slidesPerGroup: 2,
+    },
+
+    1920: {
+      slidesPerView: 3,
+    }
   },
-  spaceBetween: 50,
 
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 const swiperEvent = new Swiper('.event__swiper', {
@@ -17,24 +34,54 @@ const swiperEvent = new Swiper('.event__swiper', {
   loop: true,
   slidesPerView: 3,
 
+  breakpoints: {
+    1024: {
+      spaceBetween: 27,
+      slidesPerGroup: 3,
+    },
+
+    1920: {
+      spaceBetween: 50,
+    }
+
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
   },
-  spaceBetween: 48,
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
 
 });
+
 
 const swiperProjects = new Swiper('.projects__swiper', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
 
+  breakpoints: {
+    1024: {
+      spaceBetween: 50,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+
+    1920: {
+      spaceBetween: 50,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    }
+
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  spaceBetween: 50,
 
 });
