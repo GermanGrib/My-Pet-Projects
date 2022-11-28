81dab8e1573683aebbdbb5b07f79120b2477ac84
German Gribanov <germangribanov@mail.ru>
2022-11-28 22:11:11 +0300

    download 768 media pixel perfect

--- Blanchard/JS/swiperSlider.js
+++ Blanchard/JS/swiperSlider.js
@@ -1,11 +1,14 @@
 var swiper = new Swiper(".gallery__swiper", {
 
   loop: true,
-  slidesPerView: 3,
-  slidesPerGroup: 3,
-  spaceBetween: 49,
+  spaceBetween: 50,
 
   breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 38,
+      slidesPerGroup: 2,
+    },
 
     1024: {
       slidesPerView: 2,
@@ -15,6 +18,12 @@ var swiper = new Swiper(".gallery__swiper", {
 
     1920: {
       slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+
+    3000: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
     }
   },
 
@@ -32,15 +41,22 @@ var swiper = new Swiper(".gallery__swiper", {
 const swiperEvent = new Swiper('.event__swiper', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
 
   breakpoints: {
+    768: {
+      spaceBetween: 34,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+
     1024: {
       spaceBetween: 27,
+      slidesPerView: 3,
       slidesPerGroup: 3,
     },
 
     1920: {
+      slidesPerView: 3,
       spaceBetween: 50,
     }
 
@@ -48,12 +64,12 @@ const swiperEvent = new Swiper('.event__swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
+    nextEl: '.event__swiperbtnnext',
   },
 
   pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
+    el: '.event__pagination',
+    type: 'bullets',
     clickable: true,
   },
 
@@ -65,6 +81,12 @@ const swiperProjects = new Swiper('.projects__swiper', {
   loop: true,
 
   breakpoints: {
+    768: {
+      spaceBetween: 34,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+
     1024: {
       spaceBetween: 50,
       slidesPerView: 2,
