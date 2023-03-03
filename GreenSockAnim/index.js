323c53af99df795b5a56e5ecf9763f0c82c60d5d
German Gribanov <germangribanov@mail.ru>
2023-03-03 17:44:07 +0300

    fix: todo fix bag

--- GreenSockAnim/index.js
+++ GreenSockAnim/index.js
@@ -24,20 +24,17 @@ gsap.delayedCall(1.8, function () {
 let tl = gsap.timeline({ paused: true })
 
 tl.to('.menu__top', { y: 0, opacity: 1, duration: .6 })
-tl.to('.menu', { backgroundColor: 'rgba(55, 55, 55, 1)', duration: .3 });
+tl.to('.menu', { backgroundColor: 'rgba(55, 55, 55, 1)', className: 'menu menu--open', duration: .3 });
 tl.fromTo('.menu__nav', { y: 30, opacity: 0 }, { y: 0, opacity: 1, duration: .3 })
 tl.fromTo('.menu__right, .social', { y: 50, opacity: 0 }, { y: 0, opacity: 1, duration: .3 })
 
-
-burgerBtn.addEventListener('click', openBurgerMenu)
 function openBurgerMenu() {
-  burgerMenu.classList.add('menu--open')
   tl.play()
 }
-
-burgerBtnClose.addEventListener('click', closeBurgerMenu)
 function closeBurgerMenu() {
   tl.reverse()
-  // burgerMenu.classList.remove('menu--open')
-  // deletes immediately classlist and reverse work uncorectly, I don't know how ti fix it
 }
+
+
+burgerBtn.addEventListener('click', openBurgerMenu)
+burgerBtnClose.addEventListener('click', closeBurgerMenu)
--- Todo/todo-app.js
+++ Todo/todo-app.js
@@ -1,5 +1,6 @@
 (function () {
   let itemsArray = []
+
   function createAppTitle(title) {
     let appTitle = document.createElement('h2')
     appTitle.innerHTML = title
@@ -29,13 +30,12 @@
 
       clearTimeout(timeoutId);
 
-      timeoutId = setTimeout(() => {
-        if (input.value.length === 0) {
-          button.setAttribute('disabled', 'disabled')
-        } else {
-          button.removeAttribute('disabled')
-        }
-      }, 300);
+      if (input.value.length === 0) {
+        button.setAttribute('disabled', 'disabled')
+      } else {
+        button.removeAttribute('disabled')
+      }
+
     });
 
     return {
@@ -78,19 +78,28 @@
       let todoThisItem = this.parentElement.parentElement
       todoThisItem.classList.toggle('list-group-item-success')
       if (todoThisItem.classList.contains('list-group-item-success')) {
-        itemsArray.find(el => el.name === todoThisItem.innerHTML.replace(/<.*/, '')).done = true
+        itemsArray.find(el => el.id === todoItemDate.id).done = true
+        console.log(itemsArray, 'IM HERE')
+        console.log(localStorKey)
+        localStorage.removeItem(localStorKey)
+        saveListLocStor(itemsArray, localStorKey)
       } else {
-        itemsArray.find(el => el.name === todoThisItem.innerHTML.replace(/<.*/, '')).done = false
+        itemsArray.find(el => el.id === todoItemDate.id).done = false
+        localStorage.removeItem(localStorKey)
+        saveListLocStor(itemsArray, localStorKey)
       }
+
     });
 
     deleteButton.addEventListener('click', function () {
       let todoThisItem = this.parentElement.parentElement
       if (confirm('Вы уверены?')) {
         todoThisItem.remove()
-        itemsArray = itemsArray.filter(el => el.name !== todoThisItem.innerHTML.replace(/<.*/, ''))
-        localStorage.removeItem('my' || 'mom' || 'dad')
-        localStorage.setItem(localStorKey, JSON.stringify(itemsArray))
+        itemsArray = itemsArray.filter(el => (el.id !== todoItemDate.id))
+        localStorage.removeItem(localStorKey)
+        console.log(itemsArray, 'HERE IS A PROBLEM')
+        console.log(localStorKey, 'HERE IS A KEY')
+        saveListLocStor(itemsArray, localStorKey)
       }
     })
 
@@ -101,7 +110,12 @@
     }
   }
 
+  function saveListLocStor(arr, keyName) {
+    localStorage.setItem(keyName, JSON.stringify(arr))
+  }
+
   function createTodoApp(container, title = 'Список дел', listName) {
+    console.log(listName, 'IM LIST NAME')
     let localStorKey = listName
     let todoAppTitle = createAppTitle(title)
     let todoItemForm = createTodoItemForm()
@@ -114,27 +128,33 @@
 
     todoItemForm.form.addEventListener('submit', function (e) {
       e.preventDefault();
-      let id = 1
-      let textInInput = todoItemForm.input.value
-      let todoItem = createTodoItem({ name: textInInput, done: false }, localStorKey)
+
+      let newItem = {
+        id: 0,
+        name: todoItemForm.input.value,
+        done: false,
+      };
+      if (itemsArray.length > 0) {
+        newItem.id = itemsArray[itemsArray.length - 1].id + 1
+        itemsArray.push(newItem)
+        localStorage.setItem(localStorKey, JSON.stringify(itemsArray))
+      } else {
+        itemsArray.push(newItem)
+        localStorage.setItem(localStorKey, JSON.stringify(itemsArray))
+      };
+
+      let todoItem = createTodoItem(newItem, localStorKey)
       todoList.append(todoItem.item)
       this.reset()
       document.querySelector('.btn-primary').setAttribute('disabled', 'disabled')
 
-      if (itemsArray.length > 0) {
-        id = itemsArray[itemsArray.length - 1].id + 1
-        itemsArray.push({ id, name: textInInput, done: false })
-        localStorage.setItem(localStorKey, JSON.stringify(itemsArray))
-      } else {
-        itemsArray.push({ id, name: textInInput, done: false })
-        localStorage.setItem(localStorKey, JSON.stringify(itemsArray))
-      }
+
     })
 
     if (storedItems) {
       itemsArray = JSON.parse(storedItems)
       itemsArray.forEach(el => {
-        let todoItem = createTodoItem(el, itemsArray)
+        let todoItem = createTodoItem(el, localStorKey)
         todoList.append(todoItem.item)
       })
     }
